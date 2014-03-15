1d841fa59c405ab2603ed8343af197114d8f9584
Jeffrey Camealy <jeffrey@orainteractive.com>
2014-03-15 00:34:23 -0500

    pencil tip working

--- TinyCompass/TinyCompass.m
+++ TinyCompass/TinyCompass.m
@@ -18,6 +18,9 @@ const float armThickness = 3.5;
 const float totalCompassRadius = 55.0;
 const float armCenterRadius = 10.0;
 const float screwRadius = 1.75;
+const float pencilArmTipRadius = 3.0;
+const float pencilTipRadius = 0.5;
+const float pencilAngle = degToRad(65.0);
 
 // Derived Mesurments
 const float armLength = totalCompassRadius/2.0;
@@ -27,7 +30,28 @@ const float armLength = totalCompassRadius/2.0;
 @implementation TinyCompass
 
 - (void)buildSubObjects {
-    [self.subObjects addObject:[self pinArm]];
+    // Blank
+    OSCylinder *armCenter = [[OSCylinder alloc] initWithRadius:armCenterRadius height:armThickness];
+    
+    OSCylinder *armTip = [[OSCylinder alloc] initWithRadius:pencilArmTipRadius height:armThickness];
+    OSTransformation *tipTranslation = translate(armLength, 0, 0);
+    [armTip.transformations addObject:tipTranslation];
+    
+    OSCompositeObject *armBlank = [[OSCompositeObject alloc] initWithSubObjects:@[armCenter, armTip]];
+    armBlank.compositeType = OSCTHull;
+    
+    // Holes
+    float pencilAngleTravel = sinf(M_PI_2-pencilAngle)*(armThickness/sinf(pencilAngle));
+    
+    OSTaperedCylinder *pencilHole = [[OSTaperedCylinder alloc] initWithHeight:armThickness+os_epsilon
+                                                                   baseRadius:pencilTipRadius
+                                                                    topRadius:pencilAngleTravel+pencilTipRadius];
+    [pencilHole.transformations addObject:tipTranslation];
+    
+    OSCompositeObject *armWithHoles = [[OSCompositeObject alloc] initWithSubObjects:@[armBlank, pencilHole]];
+    armWithHoles.compositeType = OSCTDifference;
+    
+    [self.subObjects addObject:armWithHoles];
 }
 
 - (OSObject *)pinArm {
