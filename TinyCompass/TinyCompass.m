2fef4ec77c89ad5667bca64a2a195ea04f4c76ec
Jeffrey Camealy <jeffrey@orainteractive.com>
2014-03-15 01:08:32 -0500

    pencin arm working

--- TinyCompass/TinyCompass.m
+++ TinyCompass/TinyCompass.m
@@ -17,7 +17,9 @@ const float pinShaftRadius = 0.375;
 const float armThickness = 3.5;
 const float totalCompassRadius = 55.0;
 const float armCenterRadius = 10.0;
-const float screwRadius = 1.75;
+const float screwShaftRadius = 1.75;
+const float screwHeadRadius = 3.75;
+const float screwHeadHeight = 3.0;
 const float pencilArmTipRadius = 3.0;
 const float pencilTipRadius = 0.5;
 const float pencilAngle = degToRad(65.0);
@@ -30,6 +32,35 @@ const float armLength = totalCompassRadius/2.0;
 @implementation TinyCompass
 
 - (void)buildSubObjects {
+
+    
+    [self.subObjects addObject:[self pencilArm]];
+}
+
+- (OSObject *)pinArm {
+    // Pin Arm
+    OSCylinder *pinArmCenter = [[OSCylinder alloc] initWithRadius:armCenterRadius height:armThickness];
+    
+    OSCylinder *pinArmTip = [[OSCylinder alloc] initWithRadius:pinCapRadius height:armThickness];
+    OSTransformation *pinArmTipTranslation = translate(armLength, 0, 0);
+    [pinArmTip.transformations addObject:pinArmTipTranslation];
+    
+    OSCompositeObject *pinArmBlank = [[OSCompositeObject alloc] initWithSubObjects:@[pinArmCenter, pinArmTip]];
+    pinArmBlank.compositeType = OSCTHull;
+    
+    // Holes
+    OSCylinder *pinHole = [[OSCylinder alloc] initWithRadius:pinShaftRadius height:armThickness+os_epsilon];
+    [pinHole.transformations addObject:pinArmTipTranslation];
+    
+    OSCylinder *screwHole = [[OSCylinder alloc] initWithRadius:screwShaftRadius height:armThickness+os_epsilon];
+    
+    OSCompositeObject *pinArmWithHoles = [[OSCompositeObject alloc] initWithSubObjects:@[pinArmBlank, pinHole, screwHole]];
+    pinArmWithHoles.compositeType = OSCTDifference;
+    
+    return pinArmWithHoles;
+}
+
+- (OSObject *)pencilArm {
     // Blank
     OSCylinder *armCenter = [[OSCylinder alloc] initWithRadius:armCenterRadius height:armThickness];
     
@@ -48,33 +79,18 @@ const float armLength = totalCompassRadius/2.0;
                                                                     topRadius:pencilAngleTravel+pencilTipRadius];
     [pencilHole.transformations addObject:tipTranslation];
     
-    OSCompositeObject *armWithHoles = [[OSCompositeObject alloc] initWithSubObjects:@[armBlank, pencilHole]];
+    
+    float screwAngle = atanf(screwHeadHeight/(screwHeadRadius-screwShaftRadius));
+    float screwHoleTravel = sinf(M_PI_2-screwAngle)*(armThickness/sinf(screwAngle));
+    
+    OSTaperedCylinder *screwHole = [[OSTaperedCylinder alloc] initWithHeight:armThickness+os_epsilon
+                                                                  baseRadius:screwHoleTravel+screwShaftRadius
+                                                                   topRadius:screwShaftRadius];
+    
+    OSCompositeObject *armWithHoles = [[OSCompositeObject alloc] initWithSubObjects:@[armBlank, screwHole, pencilHole]];
     armWithHoles.compositeType = OSCTDifference;
     
-    [self.subObjects addObject:armWithHoles];
-}
-
-- (OSObject *)pinArm {
-    // Pin Arm
-    OSCylinder *pinArmCenter = [[OSCylinder alloc] initWithRadius:armCenterRadius height:armThickness];
-    
-    OSCylinder *pinArmTip = [[OSCylinder alloc] initWithRadius:pinCapRadius height:armThickness];
-    OSTransformation *pinArmTipTranslation = translate(armLength, 0, 0);
-    [pinArmTip.transformations addObject:pinArmTipTranslation];
-    
-    OSCompositeObject *pinArmBlank = [[OSCompositeObject alloc] initWithSubObjects:@[pinArmCenter, pinArmTip]];
-    pinArmBlank.compositeType = OSCTHull;
-    
-    // Holes
-    OSCylinder *pinHole = [[OSCylinder alloc] initWithRadius:pinShaftRadius height:armThickness+os_epsilon];
-    [pinHole.transformations addObject:pinArmTipTranslation];
-    
-    OSCylinder *screwHole = [[OSCylinder alloc] initWithRadius:screwRadius height:armThickness+os_epsilon];
-    
-    OSCompositeObject *pinArmWithHoles = [[OSCompositeObject alloc] initWithSubObjects:@[pinArmBlank, pinHole, screwHole]];
-    pinArmWithHoles.compositeType = OSCTDifference;
-    
-    return pinArmWithHoles;
+    return armWithHoles;
 }
 
 
