b95c41f694c00be6f11933272204947b66db2391
Jeffrey Camealy <jeffrey@orainteractive.com>
2014-03-14 22:26:29 -0500

    Initial Commit

---
+++ TinyCompass/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  TinyCompass
+//
+//  Created by Jeffrey Camealy on 3/14/14.
+//  Copyright (c) 2014 bearMountain. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
